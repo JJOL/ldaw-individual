3b4ea0d460319eeecbc11e24f8010df1d6e85824
jjol <jjoulk@gmail.com>
2019-09-01 21:23:42 -0500

    Empezar Time Travel del Tutorial

--- tut-rapp/src/index.js
+++ tut-rapp/src/index.js
@@ -15,8 +15,8 @@ import './index.css';
 
   function Square(props) {
     return (
-      <button className="square" onClick={() => props.onClick() }>
-        {props.value}
+      <button className="square" onClick={ () => props.onClick() }>
+        { props.value }
       </button>
     );
   }
@@ -80,14 +80,41 @@ import './index.css';
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        history: [
+          { squares: Array(9).fill(null) }
+        ],
+        xIsNext: true
+      }
+    }
+
+    handleClick(i) {
+
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[history.length-1];
+
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board squares={current.squares} 
+                   onClick={(i) => this.handleClick(i) }/>
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{ status }</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
