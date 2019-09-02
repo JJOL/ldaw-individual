679e7f53b61ac7096b57d5c38dead8362b26a110
jjol <jjoulk@gmail.com>
2019-09-01 21:34:02 -0500

    Se termino Lifting State Up Again del Time Travel.

--- tut-rapp/src/index.js
+++ tut-rapp/src/index.js
@@ -22,43 +22,14 @@ import './index.css';
   }
   
   class Board extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true
-      };
-    }
-
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i])
-        return;
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-      });
-    }
-
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}
-                     onClick={() => this.handleClick(i)}/>;
+      return <Square value={this.props.squares[i]}
+                     onClick={() => this.props.onClick(i)}/>;
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -92,14 +63,28 @@ import './index.css';
     }
 
     handleClick(i) {
+      const history = this.state.history;
+      const current = history[history.length-1];
+      const squares = current.squares.slice();
 
+      if (calculateWinner(squares) || squares[i])
+        return;
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+      this.setState({
+        history: history.concat([{
+          squares: squares
+        }]),
+        xIsNext: !this.state.xIsNext
+      });
     }
 
     render() {
       const history = this.state.history;
       const current = history[history.length-1];
 
-      const winner = calculateWinner(this.state.squares);
+      const winner = calculateWinner(current.squares);
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
